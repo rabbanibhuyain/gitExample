a03ccdc104f4c18a279f49722cf639b55ff38463
Rabbani <rabbani.bhuyain@outlook.com>
2020-07-12 20:58:15 -0400

    new chages

--- src/io/rabbani/Main.java
+++ src/io/rabbani/Main.java
@@ -5,4 +5,25 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("This is git testing");
     }
+
+    public void TestCase01(){
+        System.out.println("TestCase01");
+    }
+
+    public void TestCase02(){
+        System.out.println("TestCase02");
+    }
+
+
+
+
 }
+
+
+
+
+
+
+
+
+
--- src/io/rabbani/SingInPage.java
+++ src/io/rabbani/SingInPage.java
@@ -3,10 +3,12 @@ package io.rabbani;
 public class SingInPage {
 
     public void createNewAccountTest(){
+
         System.out.println("createNewAccountTest");
     }
 
     public void signinTest(){
+
         System.out.println("signinTest");
     }
 }
