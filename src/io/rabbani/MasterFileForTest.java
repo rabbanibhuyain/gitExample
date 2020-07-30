8488e9f5fb2ff4130a569a23677f7d9a87271f5c
Rabbani <Rabbani.Bhuyain@insight.com>
2020-07-29 20:31:59 -0400

    another updated

--- src/io/rabbani/MasterFileForTest.java
+++ src/io/rabbani/MasterFileForTest.java
@@ -2,4 +2,7 @@ package io.rabbani;
 
 public class MasterFileForTest {
     //TODO: @shamim will you please implement this
+    public void newFunction(){
+        System.out.println("New Feature!");
+    }
 }
